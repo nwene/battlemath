a7590f3197a08903b589783109bb464e7e993ab6
nwene <nwenedokus@gmail.com>
2019-04-01 23:04:55 +0100

    <commit message>

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {StyleSheet, Text, View, TextInput, Button} from "react-native";
+import './App.css';
+
+class App extends Component {
+  state =  {
+    text: ''
+  }
+  render() {
+    return (
+      <View style={styles.root}>
+        <Text>Battle Math</Text>
+        <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
+      <Button
+        onPress={() => console.log(this.state.text)}
+        title="Submit"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  root:{
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+export default App;
